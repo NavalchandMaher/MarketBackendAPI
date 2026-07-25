b48c30f7ea3357f0c5a354a2ad52fd84bec81a94
NavalchandMaher <39883480+NavalchandMaher@users.noreply.github.com>
2026-07-25 19:03:15 +0530

    Code Cleanup Step1

--- test_ai.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-
-client = OpenAI(api_key="OPENAI_API_KEY")
-
-res = client.chat.completions.create(
-    model="gpt-4.1-mini", messages=[{"role": "user", "content": "Say hello"}]
-)
-
-print(res.choices[0].message.content)
